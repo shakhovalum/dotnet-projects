15524e9705b41c3526e6df86dfedc92f06c5f0ea
Iuliia Shakhova <Iuliia_Shakhova@epam.com>
2025-03-17 11:18:43 +0100

    Task 1 initial version

---
+++ ISHCartingService/ISHCartingService/ISHCartingServiceDAL/CartingDataManager.cs
@@ -0,0 +1,65 @@
+﻿using MongoDB.Driver;
+using ISHCartingService.ISHCartingServiceModels;
+using MongoDB.Bson;
+
+namespace ISHCartingService.ISHCartingServiceDAL
+{
+    public class CartingDataManager
+    {
+        private readonly MongoDBContext _context;
+
+        public CartingDataManager(MongoDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Cart> GetCartAsync(string cartId)
+        {
+            return await _context.Carts.Find(cart => cart.Id == cartId).FirstOrDefaultAsync();
+        }
+
+        public async Task AddItemToCartAsync(string cartId, CartItem item)
+        {
+            var filter = Builders<Cart>.Filter.Eq(c => c.Id, cartId);
+
+            var arrayFilters = new[]
+            {
+                new BsonDocumentArrayFilterDefinition<BsonDocument>(new BsonDocument("i._id", item.Id))
+            };
+
+            var update = Builders<Cart>.Update
+                .Set("Items.$[i].Name", item.Name)
+                .Set("Items.$[i].ImageUrl", item.ImageUrl)
+                .Set("Items.$[i].ImageAltText", item.ImageAltText)
+                .Set("Items.$[i].Price", item.Price)
+                .Set("Items.$[i].Quantity", item.Quantity);
+
+            var updateResult = await _context.Carts.UpdateOneAsync(filter, update, new UpdateOptions { ArrayFilters = arrayFilters });
+
+            if (updateResult.ModifiedCount == 0)
+            {
+                var pushUpdate = Builders<Cart>.Update.Push(c => c.Items, item);
+                await _context.Carts.UpdateOneAsync(filter, pushUpdate);
+            }
+        }
+
+        public async Task RemoveItemFromCartAsync(string cartId, int itemId)
+        {
+            var filter = Builders<Cart>.Filter.Eq(c => c.Id, cartId);
+            var update = Builders<Cart>.Update.PullFilter(c => c.Items, i => i.Id == itemId);
+
+            await _context.Carts.UpdateOneAsync(filter, update);
+        }
+
+        public async Task CreateNewCartAsync(string cartId)
+        {
+            var cart = new Cart
+            {
+                Id = cartId,
+                Items = new List<CartItem>()
+            };
+
+            await _context.Carts.InsertOneAsync(cart);
+        }
+    }
+}
---
+++ ISHCartingService/ISHCartingService/Program.cs
@@ -0,0 +1,95 @@
+﻿using ISHCartingService.ISHCartingServiceDAL;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using ISHCartingService.ISHCartingServiceModels;
+
+
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            var host = CreateHostBuilder(args).Build();
+            var config = host.Services.GetRequiredService<IConfiguration>();
+            var connectionString = config.GetSection("ConnectionString:MongoDB").Value;
+            Console.WriteLine($"MongoDB Connection String: {connectionString}");
+
+            MongoDBContext context;
+
+            context = new MongoDBContext(connectionString);
+
+            var dataManager = new CartingDataManager(context);
+
+
+            var cartItem1 = new CartItem
+            {
+                Id = 777,
+                Name = "Item 777",
+                ImageUrl = "http://dummy.com/item777.jpg",
+                ImageAltText = "Image 777",
+                Price = 100,
+                Quantity = 4
+            };
+
+            var cartItem2 = new CartItem
+            {
+                Id = 777,
+                Name = "Item 777 new val",
+                ImageUrl = "http://dummy.com/item777.jpg",
+                ImageAltText = "Image 777_new_02",
+                Price = 10044,
+                Quantity = 498
+            };
+
+            var cartItem3 = new CartItem
+            {
+                Id = 778,
+                Name = "Item 778",
+                ImageUrl = "http://dummy.com/item778.jpg",
+                ImageAltText = "Image 778",
+                Price = 177,
+                Quantity = 98
+            };
+
+            await dataManager.CreateNewCartAsync("878788");
+
+            await dataManager.AddItemToCartAsync("878788", cartItem1);
+
+            await dataManager.AddItemToCartAsync("878788", cartItem2);
+
+            await dataManager.AddItemToCartAsync("878788", cartItem3);
+
+            await dataManager.RemoveItemFromCartAsync("878788", 778);           
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    config.SetBasePath(Directory.GetCurrentDirectory());
+                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders(); 
+                    logging.AddConsole(); 
+                });
+
+}
+
