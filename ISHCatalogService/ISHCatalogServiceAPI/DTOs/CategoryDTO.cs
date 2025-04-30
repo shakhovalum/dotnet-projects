0c552a150c3b7a4eca7ee7d647b4ee8cc072eeed
Iuliia Shakhova <Iuliia_Shakhova@epam.com>
2025-04-30 10:11:59 +0200

    RESTful Web API: Catalog API

---
+++ ISHCatalogService/ISHCatalogServiceAPI/DTOs/CategoryDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ISHCatalogServiceAPI.DTOs
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public int? ParentCategoryId { get; set; }
+    }
+}
